ae8c2e290105e089fada9aa3b30574529a90b70d
iamSK77 <sk77.mobilefix@gmail.com>
2020-09-26 12:36:38 +0530

    final

--- gulpfile.js
+++ gulpfile.js
@@ -10,22 +10,14 @@ const sourcemaps = require('gulp-sourcemaps');
 // gulp plugin for concatenation of files
 const concat = require('gulp-concat');
 
-// gulp plugin
+// gulp sass
 const sass = require('gulp-sass');
-const browserSync = require('browser-sync');
-
-// gulp plugin uglify
-const uglify = require('gulp-uglify');
 
 // destructered gulp object
 const { src, series, parallel, dest, watch } = require('gulp');
-const cssnano = require('cssnano');
 
 // declared image path variable
 const imgPath = 'images/**/*';
-
-// copies HTML and distributed to final folder
-const copyHTML = () => src('*.html').pipe(gulp.dest('final'));
 
 // copies and minimises size of the images
 // const imgTasks = () => src('images/*').pipe(imagemin()).pipe(gulp.dest('final/images'));
@@ -33,38 +25,26 @@ const imgTask = () => {
     return src(imgPath)
         .pipe(sourcemaps.init())
         .pipe(imagemin())
-        .pipe(dest('final/images'));
-}
+        .pipe(dest('dist/images'));
+};
+
+gulp.task('imgTask', imgTask);
+
+gulp.task('watch', () => {
+  gulp.watch('imgTask', imgTask);
+});
 
 // minifies (uglify) SCSS
 const sassTask = () => {
     return gulp.src('styles/**/*.scss')
-    //   .pipe(sourcemaps.init())
-      .pipe(sass())
-    //   .pipe(sourcemaps.write())
-      .pipe(gulp.dest('final/assets/css'));
-};
-
-const cssTask = () => {
-    return gulp.src('final/assets/css/main.css')
-    .pipe(cssnano())
-    .pipe(gulp.dest('final/assets/css'));
-}
-
-// minifies (uglify) CSS
-const jsTask = () => {
-    return gulp.src('script/*.js')
       .pipe(sourcemaps.init())
-      .pipe(uglify())
+      .pipe(sass())
       .pipe(sourcemaps.write())
-      .pipe(dest('final/assets/js'));
+      .pipe(gulp.dest('dist/css'));
 };
 
+gulp.task("default",gulp.series("imgTask","watch"));
 
 
-
-exports.jsTask = jsTask;
-exports.cssTask = cssTask;
 exports.sassTask = sassTask;
 exports.imgTask = imgTask;
-exports.copyHTML = copyHTML;
